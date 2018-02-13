d5b15c7d0a049c7e716d96528a817cbba29ba829
Tyler Brandt <tylerbrandt02@gmail.com>
2018-02-12 23:17:14 -0600

    PlayGame Function
    Added PlayGame function and method for door class in S4.

--- PS3_Brandt.R
+++ PS3_Brandt.R
@@ -2,6 +2,8 @@
 
 # S3 system
 # Define doors
+rm(list = ls())
+
 one <- 1
 class(one) <- "door"
 two <- 2
@@ -27,6 +29,8 @@ PlayGame(one)
 
 # S4 system
 ## Define Doors
+rm(list = ls())
+
 setClass(Class = "door",
          representation = representation(
            x = "numeric"
@@ -35,6 +39,7 @@ setClass(Class = "door",
            x = 1
          ))
 
+# Ensure values of 1,2, or 3
 setValidity("door", function(object){
   if (object @ x != 1 & object @ x != 2 & object@ x != 3){
     return ("@x value for door object must be in {1,2,3}")
@@ -43,3 +48,24 @@ setValidity("door", function(object){
 
 new("door", x = 4)
 new("door", x = 3)
+
+# Create Generic PlayGame function
+setGeneric("PlayGame",
+           function(object){
+             standardGeneric("PlayGame")
+           })
+
+# Create Door Method for PlayGame function
+setMethod("PlayGame", "door",
+          function (object){
+            random_door <- new("door", x = sample(1:3,1))
+            if (object@x == random_door@x){
+              print ("You won a new car!")
+            } else {
+              print ("Sorry, you will have to settle for a goat")
+            }
+          })
+
+# Test function
+one <- new("door", x = 1)
+PlayGame(one)
